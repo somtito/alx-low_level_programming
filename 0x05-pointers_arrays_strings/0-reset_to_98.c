3b1245846aa0ebb3f1f55a14999b5708b696e86b
somtito <somyfobis>
2023-03-28 05:30:15 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
-#include" main.h"
+#include "main.h"
 /**
  * reset_to_98 - a function that updates the value it points to
  * @n: input
  * Return: n
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n) 
 {
 	*n = 98;
 }

9d1e6838b19164a0339c4a61bb0d77283025aebe
somtito <somyfobis>
2023-03-28 18:50:02 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,13 @@ void puts2(char *str)
 {
 	int j = 0;
 	int i;
-	
+
 	while (str[j] != '\0')
 	{
 		j++;
 
 	}
-	for ( i = 0; i < j; i += 2)
+	for (i = 0; i < j; i += 2)
 	{
 		_putchar(str[i]);
 	}
